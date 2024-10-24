65262350b328039ec2de150cfe796c5d053ae8e2
Radheshyam Rathore <rsrathore002@gmail.com>
2024-10-24 15:12:18 +0530

    added two factor authentication or login and signup via linkdin

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def linkedin
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "LinkedIn") if is_navigational_format?
+    else
+      session["devise.linkedin_data"] = request.env["omniauth.auth"].except(:extra)
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to new_user_session_path
+  end
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,22 @@
-# app/models/user.rb
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :two_factor_authenticatable,
+         :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:linkedin] # This is the correct placement
 
   enum role: [:employee, :hr_manager]
-  
-  scope :available_for_employee_assignment, -> { where(role: "employee").left_outer_joins(:employee).where(employees: { id: nil }) }
 
   has_one :employee
-  
+
   after_initialize :set_default_role, if: :new_record?
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+    end
+  end
 
   private
 
